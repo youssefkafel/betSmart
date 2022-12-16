a47fd16cc0fac23fe0b414fb089679c598e507fa
Youssef Kafel <45083807+youssefkafel@users.noreply.github.com>
2022-12-15 19:11:27 -0500

    Update settings.py

--- betSmart/settings.py
+++ betSmart/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATIC_URL = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
